973d7185faea29c884a0d6470b9604118c68bb9b
Edgar Blau <edgar.blau@usach.cl>
2018-12-07 15:23:50 -0300

    Migracion rol.

--- database/migrations/2018_12_07_174739_rol.php
+++ database/migrations/2018_12_07_174739_rol.php
@@ -13,7 +13,11 @@ class Rol extends Migration
      */
     public function up()
     {
-        //
+      Schema::create('rol', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('tipo');
+          $table->timestamps();
+      });
     }
 
     /**
@@ -23,6 +27,6 @@ class Rol extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('rol');
     }
 }
