5f9fabccc536fbfafb2430bbeea7627b449c024a
Edgar Blau <edgar.blau@usach.cl>
2018-12-07 14:38:45 -0300

    agregando archivos faltantes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('activate/{token}', 'Auth\RegisterController@activate')
+    ->name('activate');
+Route::get('/home', 'HomeController@index')->name('home');
