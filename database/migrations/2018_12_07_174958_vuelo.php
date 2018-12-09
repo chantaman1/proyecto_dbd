fb8da67231f85885a51a8476e46c5302caf595cf
Edgar Blau <edgar.blau@usach.cl>
2018-12-09 17:57:11 -0300

    agregado datos faltantes.

--- database/migrations/2018_12_07_174958_vuelo.php
+++ database/migrations/2018_12_07_174958_vuelo.php
@@ -13,7 +13,7 @@ class Vuelo extends Migration
      */
     public function up()
     {
-      Schema::create('reserva', function (Blueprint $table) {
+      Schema::create('vuelo', function (Blueprint $table) {
           $table->increments('id');
           $table->integer('codigo');
           $table->string('tipo');
@@ -32,6 +32,6 @@ class Vuelo extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('vuelo');
     }
 }
