284d690f70f2d2f96cc9b23c8c4a8f3c8ab51f0c
José Mellado <jose.mellado@usach.cl>
2018-12-12 18:57:20 -0300

    Se hizo el primer modelo completo (Reserva)

--- app/Usuario.php
+++ app/Usuario.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
-    //
+  //El usuario hace muchas reservas
+  public function reservas()
+  {
+      return $this->hasMany('App\Reserva', 'reservable');
+  }
 }
