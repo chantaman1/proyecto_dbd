96e476d2559da8aecc9a3b00e5a623c510e647d9
José Mellado <jose.mellado@usach.cl>
2018-12-13 18:05:40 -0300

    Se agregaron atributos intermedios en las relaciones reserva-habitacion y reserva-vehiculo.

--- database/migrations/2018_12_09_203558_reserva_vehiculo.php
+++ database/migrations/2018_12_09_203558_reserva_vehiculo.php
@@ -15,6 +15,10 @@ class ReservaVehiculo extends Migration
     {
       Schema::create('reserva_vehiculos', function (Blueprint $table) {
         $table->increments('id');
+        $table->date('fecha_inicio');
+        $table->time('hora_inicio');
+        $table->date('fecha_termino');
+        $table->time('hora_termino');
         $table->unsignedInteger('id_reserva');
         $table->unsignedInteger('id_vehiculo');
         $table->foreign('id_reserva')->references('id')->on('reservas')->onDelete('cascade');
--- database/migrations/2018_12_09_203606_reserva_habitacion.php
+++ database/migrations/2018_12_09_203606_reserva_habitacion.php
@@ -15,6 +15,8 @@ class ReservaHabitacion extends Migration
     {
       Schema::create('reserva_habitacions', function (Blueprint $table) {
         $table->increments('id');
+        $table->date('fecha_inicio');
+        $table->date('fecha_termino');
         $table->unsignedInteger('id_reserva');
         $table->unsignedInteger('id_habitacion');
         $table->foreign('id_reserva')->references('id')->on('reservas')->onDelete('cascade');
