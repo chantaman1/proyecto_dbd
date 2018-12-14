c04f99782c106787ac84da8efc0e154aae823118
Nicolás Cárcamo <nicolas.carcamo.f@usach.cl>
2018-12-14 16:13:01 -0300

    Tablas Seeder incompletas (tambien sin tablas intermedias)

---
+++ database/seeds/reservaTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class reservaTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
