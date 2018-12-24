0ce0948462a604fb82293ebd38f770fa2d2ec56e
Nicolás Cárcamo <nicolas.carcamo.f@usach.cl>
2018-12-24 17:28:44 -0300

    Se agregaron más aerolineas a aerolineaTableSeeder

--- database/seeds/aerolineaTableSeeder.php
+++ database/seeds/aerolineaTableSeeder.php
@@ -54,7 +54,463 @@ class aerolineaTableSeeder extends Seeder
       ]);
 
       DB::table('aerolineas')->insert([
-          'nombre' => "Sky Airlines",
+          'nombre' => "AirAsia",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Singapore Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "ANA All Nipon Airways ",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Cathay Pacific",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "EVA Air",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Lufthansa",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Etihad Airways",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Hainan Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Garuda Indonesia",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Thai Airways",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Turkish Airways",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Virgin Australia",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Qantas Airways",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Japan Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Austrian",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Air France",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Air New Zealand",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Asiana Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Bangkok Airways",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "KLM",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "China Southern",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Hong Kong Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Finnair",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Cathay Dragon",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Norwegian",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Air Canada",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Aeroflot",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Malaysia Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Delta Air Lines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Virgin Atlantic",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Korean Air",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Alaska Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Aegean Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Aer Lingus",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "jetBlue Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "British Airways",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "easyJet",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Iberia",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Virgin America",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Jetstar Airways",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Vietnam Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "South African Airways",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Avianca",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Saudi Arabian Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Azul Brazilian",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Oman Air",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Southwest Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Porter Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "SilkAir",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Indigo",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "WetJet",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Scoot",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Alitalia",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Thomson Airways",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Philippine Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Brussels Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Copa Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Azerbaijan Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "China Eastern",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Jet Airways",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Ryanair",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "United Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "SriLankan Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Icelandair",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Gulf Air",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Air Berlin",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Vueling",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Hawaiian Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Air Nostrum",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Tigerair",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Kenya Airways",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Royal Brunei Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Air Malta",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Xiamen Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "S7 Airlines",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Peach",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aerolineas')->insert([
+          'nombre' => "Royal Air Maroc",
           'created_at' => now(),
           'updated_at' => null,
       ]);
