e54c682181b8d77af02d8dbb79fb8786eb70639c
Nicolás Cárcamo <nicolas.carcamo.f@usach.cl>
2018-12-24 18:56:52 -0300

    Más vehículos en (falta descripcion y compania id) vehículosSeed

--- database/seeds/vehiculoTableSeeder.php
+++ database/seeds/vehiculoTableSeeder.php
@@ -11,6 +11,116 @@ class vehiculoTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+      DB::table('vehiculos')->insert([
+          'patente' => 'CGHJ56',
+          'marca' => 'Suzuki',
+          'modelo' => 'Swift',
+          'año' => '2017',
+          'precio' => '$20.823',
+          'cantidad_asientos' => '5',
+          'tipo_transmision' => 'mecanico',
+          'descripcion' => '',
+          'compania_alquiler_id' => '',
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('vehiculos')->insert([
+          'patente' => 'CDTS34',
+          'marca' => 'Hyundai',
+          'modelo' => 'Grand I-20',
+          'año' => '2016',
+          'precio' => '$20.823',
+          'cantidad_asientos' => '5',
+          'tipo_transmision' => 'mecanico',
+          'descripcion' => '',
+          'compania_alquiler_id' => '',
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('vehiculos')->insert([
+          'patente' => 'DRGT57',
+          'marca' => 'Toyota',
+          'modelo' => 'All New Yaris Sport',
+          'año' => '2018',
+          'precio' => '$29.237',
+          'cantidad_asientos' => '5',
+          'tipo_transmision' => 'mecanico',
+          'descripcion' => '',
+          'compania_alquiler_id' => '',
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('vehiculos')->insert([
+          'patente' => 'BSTP43',
+          'marca' => 'Hyundai',
+          'modelo' => 'Accent',
+          'año' => '2017',
+          'precio' => '$29.237',
+          'cantidad_asientos' => '5',
+          'tipo_transmision' => 'mecanico',
+          'descripcion' => '',
+          'compania_alquiler_id' => '',
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('vehiculos')->insert([
+          'patente' => 'CRPR22',
+          'marca' => 'Peugeot',
+          'modelo' => '208',
+          'año' => '2017',
+          'precio' => '$29.237',
+          'cantidad_asientos' => '5',
+          'tipo_transmision' => 'mecanico',
+          'descripcion' => '',
+          'compania_alquiler_id' => '',
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('vehiculos')->insert([
+          'patente' => 'DRTW28',
+          'marca' => 'Peugeot',
+          'modelo' => '208 Active',
+          'año' => '2018',
+          'precio' => '$30.699',
+          'cantidad_asientos' => '5',
+          'tipo_transmision' => 'mecanico',
+          'descripcion' => '',
+          'compania_alquiler_id' => '',
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('vehiculos')->insert([
+          'patente' => 'BPTR17',
+          'marca' => 'Hyundai',
+          'modelo' => 'Elantra',
+          'año' => '2018',
+          'precio' => '$38.996',
+          'cantidad_asientos' => '5',
+          'tipo_transmision' => 'automatico',
+          'descripcion' => '',
+          'compania_alquiler_id' => '',
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('vehiculos')->insert([
+          'patente' => 'CVGF25',
+          'marca' => 'Chevrolet',
+          'modelo' => 'Prisma',
+          'año' => '2018',
+          'precio' => '$36.881',
+          'cantidad_asientos' => '5',
+          'tipo_transmision' => 'mecanico',
+          'descripcion' => '',
+          'compania_alquiler_id' => '',
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
     }
 }
