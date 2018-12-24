6237bc8afda6aa892379802532ff98a5edcd0f39
Nicolás Cárcamo <nicolas.carcamo.f@usach.cl>
2018-12-24 18:34:01 -0300

    Más aseguradoras en aseguradorasSeed

--- database/seeds/aseguradoraTableSeeder.php
+++ database/seeds/aseguradoraTableSeeder.php
@@ -70,5 +70,102 @@ class aseguradoraTableSeeder extends Seeder
           'created_at' => now(),
           'updated_at' => null,
       ]);
+
+      DB::table('aseguradoras')->insert([
+          'nombre' => 'Arch Roam Right',
+          'direccion' => '11350 McCormick Road',
+          'telefono' => '+54 1 800 699 3845',
+          'ciudad' => 'Cockeysville',
+          'pais' => 'United States',
+          'webpage' => 'https://www.roamright.com',
+          'activo' => true,
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aseguradoras')->insert([
+          'nombre' => 'Generali Global Assistance',
+          'direccion' => '4181 Ruffin Rd #150',
+          'telefono' => '+54 800 541 3522',
+          'ciudad' => 'San Diego',
+          'pais' => 'United States',
+          'webpage' => 'https://www.generalitravelinsurance.com',
+          'activo' => true,
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aseguradoras')->insert([
+          'nombre' => 'Medjet',
+          'direccion' => '3075 Healthy Way',
+          'telefono' => '+54 800 527 7478',
+          'ciudad' => 'Birmingham',
+          'pais' => 'United States',
+          'webpage' => 'https://https://medjetassist.com',
+          'activo' => true,
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aseguradoras')->insert([
+          'nombre' => 'Allianz Global Assistance',
+          'direccion' => 'P.O. Box 71533',
+          'telefono' => '+ 54 1 866 724 5082',
+          'ciudad' => 'Richmond',
+          'pais' => 'United States',
+          'webpage' => 'https://www.allianztravelinsurance.com',
+          'activo' => true,
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aseguradoras')->insert([
+          'nombre' => 'Seven Corners, Inc.',
+          'direccion' => '303 Congressional Boulevard',
+          'telefono' => '+54 1 800 335 0611',
+          'ciudad' => 'Carmel',
+          'pais' => 'United States',
+          'webpage' => 'https://www.sevencorners.com/es/',
+          'activo' => true,
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aseguradoras')->insert([
+          'nombre' => 'AardvarkInsure.com Corporation',
+          'direccion' => '1200 South Pine Island Road',
+          'telefono' => '+54 650 397 6590',
+          'ciudad' => 'Plantation',
+          'pais' => 'United States',
+          'webpage' => 'https://www.aardvarkcompare.com',
+          'activo' => true,
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aseguradoras')->insert([
+          'nombre' => 'Travelex Insurance Services',
+          'direccion' => '9140 West Dodge Road',
+          'telefono' => '+54 1 800 228 9792 ',
+          'ciudad' => 'Omaha',
+          'pais' => 'United States',
+          'webpage' => 'https://www.travelexinsurance.com',
+          'activo' => true,
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aseguradoras')->insert([
+          'nombre' => 'Atlas Travel Insurance',
+          'direccion' => '251 North Illinois Street',
+          'telefono' => '+54 800 605 2282',
+          'ciudad' => 'Indianapolis',
+          'pais' => 'United States',
+          'webpage' => 'https://www.hccmis.com',
+          'activo' => true,
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+      
     }
 }
