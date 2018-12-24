2fe33fc4fe4d1fe366a3edd05c047221e1e1357d
Nicolás Cárcamo <nicolas.carcamo.f@usach.cl>
2018-12-24 17:57:41 -0300

    Más aeropuertos a aeropuertoSeed

--- database/seeds/aeropuertoTableSeeder.php
+++ database/seeds/aeropuertoTableSeeder.php
@@ -58,5 +58,461 @@ class aeropuertoTableSeeder extends Seeder
           'created_at' => now(),
           'updated_at' => null,
       ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Singapore Changi Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Incheon International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Tokyo International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Hong Kong International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Doha Hamad International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Munich Airpoirt",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Central Japan International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "London Heathrow Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Zurich Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Frankfurt Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Narita International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Amsterdam Schiphol Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Kansai International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Vancouver International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Taiwan Taoyuan International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Helsinki-Vantaa Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Vienna International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Copenhagen Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Sydney Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Cape Town International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Brisbane Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Dubai International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Auckland International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Hamburg Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Melbourne Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "London City Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Denver International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Dusseldorf Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Cologne Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Cologne Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Beijing Capital International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Athens International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Gimpo International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Toronto Pearson International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Barceloma El Prat Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Madrid-Barajas Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Kuala Lumpur International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Bogota El Dorado International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Quito International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Houston George Bush Intercontinental Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Lima Jorge Chavez International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "San Francisco International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Christchurch International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Abu Dhabi International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "London Gatwick Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Gold Coast Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Lisbon Portela Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Stockholm Arlanda Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Perth Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Chengdu Shuangliu International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Oslo Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Mumbai Chhatrapati Shivaji International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Bengaluru International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Baku Heydar Aliyev International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "New York JFK International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Guayaquil International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Porto Francisco Sa Carneiro Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Los Angeles International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Bahrai International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Malta International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Moscow Domodedovo International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Adelaide Airport ",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Dublin Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Sanya Phoenix International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Hanoi Noi Bai International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Phoenix Sky Harbor International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Rome Leonardo da Vinci Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Birmingham Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Billund Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Boston Logan International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Budapest International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Brussels Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Warsaw Chopin Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Medina International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Fukuoka Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
+
+      DB::table('aeropuertos')->insert([
+          'nombre' => "Mauritius International Airport",
+          'created_at' => now(),
+          'updated_at' => null,
+      ]);
     }
 }
