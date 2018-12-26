967b269aa448f83ea0e73db91509d5e962a6cb49
josemellado <jose.mellado@usach.cl>
2018-12-26 20:16:29 -0300

    Ruteando index y show de cada controlador

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,23 @@ Route::get('activate/{token}', 'Auth\RegisterController@activate')
     ->name('activate');
 Route::get('/home', 'HomeController@index')->name('home');
 
+//RUTAS DEL USUARIO
 Route::get('/usuario/show/{id}', 'usuarioController@show');
 
 Route::get('/usuario/all/', 'usuarioController@index');
 
+//RUTAS DE LA AEROLINEA
+Route::get('/aerolinea/show/{id}', 'aerolineaController@show');
+
+Route::get('/aerolinea/all/', 'aerolineaController@index');
+
+//RUTAS DE LA AEROPUERTO
+Route::get('/aeropuerto/show/{id}', 'aeropuertoController@show');
+
+Route::get('/aeropuerto/all/', 'aeropuertoController@index');
+
+
+//-------------------------------------------------------------
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
