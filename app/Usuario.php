650926b6fee866585f12b7dc96e07567a9b877f2
josemellado <jose.mellado@usach.cl>
2018-12-26 21:20:51 -0300

    cambio modelo de Usuario a Auth

--- app/Usuario.php
+++ app/Usuario.php
@@ -2,10 +2,14 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Usuario extends Model
+class Usuario extends Authenticatable
 {
+  use Notifiable;
+
     protected $table = 'usuarios';
       protected $nombre;
       protected $apellido_paterno;
