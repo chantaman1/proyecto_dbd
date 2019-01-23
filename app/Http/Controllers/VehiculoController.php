69c31913dc9e42d341164d41bf038d783d515fa3
josemellado <jose.mellado@usach.cl>
2019-01-22 22:19:30 -0300

    Creada función para obtener vehiculos disponibles en vehiculocontroller

--- app/Http/Controllers/VehiculoController.php
+++ app/Http/Controllers/VehiculoController.php
@@ -107,4 +107,44 @@ class VehiculoController extends Controller
         return "Vehiculo no existente.";
       }
     }
+
+    public function filter(Request $request){
+      $validator = Validator::make($request->all(), [
+            'fecha_arriendo' => 'required|date|after:today',
+            'fecha_devolucion' => 'required|date|after:fecha_arriendo',
+        ]);
+    }
+    if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator);
+        }
+    $companias_validas = array();
+    foreach (Compania_alquiler::all() as $compania) {
+      if($compania->$ciudad == $request->$ciudad){
+          $companias_validas[] = $compania->$id;
+      }
+    }
+
+    $id_vehiculos = Vehiculo::whereIn('compania_alquiler_id', $companias)
+            ->pluck('id');
+    //Se obtienen todos los vehiculos que ya están reservados en la fecha solicitada
+    $reservados = Reserva::join('reserva_vehiculo', 'reserva_vehiculo.reserva_id', '=', 'reservas.id')
+    ->whereIn('reserva_vehiculo.vehiculo_id',$id_vehiculos)
+    ->where('reserva_vehiculo.fecha_inicio','<',$request->$fecha_devolucion)
+    ->where('reserva_vehiculo.fecha_termino','>',$request->fecha_arriendo)
+    ->pluck('reserva_vehiculo.vehiculo_id')
+    //Se obtienen todos los vehiculos que fueron arrendados para un paquete ene la fecha solicitada
+    $enpaquetados = Paquete::join('paquete_vehiculo','paquete_vehiculo.paquete_id','=','´paquetes.id')
+    ->whereIn('paquete_vehiculo.vehiculo_id',$id_vehiculos)
+    ->where('paquete_vehiculo.fecha_inicio','<',$request->$fecha_devolucion)
+    ->where('paquete_vehiculo.fecha_termino','>',$request->fecha_arriendo)
+    ->pluck('paquete_vehiculo.vehiculo_id')
+    //Se filtran los vehiculos disponibles, descartando los ya reservados o que fueron vendidos en un paquete
+    $vehiculos_encontrados = Vehiculo::whereIn('id',$id_vehiculos)
+    ->whereNotIn('id',$reservados)
+    ->whereNotIn('id',$enpaquetados)
+    ->get();
+
+
+
 }
