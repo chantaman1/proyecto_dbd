d2d8ec2037200f41ae43bccf8a27d2e523ce1dc7
José Mellado <jose.mellado@usach.cl>
2019-03-04 12:16:51 -0300

    update

--- resources/views/autos.blade.php
+++ resources/views/autos.blade.php
@@ -1,11 +1,35 @@
 @extends('layouts.main')
 
 @section('selected')
-	<li class="nav-item"><a class="nav-link" href="/vuelos">Vuelos</a></li>
-	<li class="nav-item"><a class="nav-link" href="/hoteles">Hoteles</a></li>
-	<li class="nav-item"><a class="nav-link" href="/paquetes">Paquetes</a></li>
-	<li class="nav-item active"><a class="nav-link" href="/autos">Autos</a></li>
-	<li class="nav-item"><a class="nav-link" href="/login">Iniciar Sesión</a></li>
+<li class="nav-item">
+	<a class="nav-link" href="/vuelos">Vuelos <span class="sr-only">(current)</span></a>
+</li>
+<li class="nav-item">
+	<a class="nav-link" href="/hoteles">Hoteles</a>
+</li>
+<li class="nav-item">
+	<a class="nav-link" href="/paquetes">Paquetes</a>
+</li>
+<li class="nav-item">
+	<a class="nav-link" href="/autos">Autos</a>
+</li>
+@if(auth()->check())
+	<li class="nav-item dropdown dmenu">
+		<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+			Bienvenido {{ Session::get('usuario_nombre') }}
+		</a>
+		<div class="dropdown-menu sm-menu">
+			<a class="dropdown-item" href="/buyHistory">Historial de compras</a>
+			<a class="dropdown-item" href="/checkin">Check-in</a>
+			<div class="dropdown-divider"></div>
+			<a class="dropdown-item" href="/logout">Cerrar sesion</a>
+		</div>
+	</li>
+@else
+	<li class="nav-item">
+		<a class="nav-link" href="/login">Iniciar sesion</a>
+	</li>
+@endif
 @endsection
 
 @section('content')
