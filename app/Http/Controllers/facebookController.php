1c4db8aa1b204d17a79f3cd2ed83e71e8119b95c
Edgar Blau <edgar.blau@usach.cl>
2019-03-04 12:04:44 -0300

    update.

--- app/Http/Controllers/facebookController.php
+++ app/Http/Controllers/facebookController.php
@@ -16,13 +16,15 @@ class facebookController extends Controller
       return Socialite::driver('facebook')->redirect();
   }
 
-  public function handleProviderFacebookCallback()
+  public function handleProviderFacebookCallback(Request $request)
   {
       $auth_user = Socialite::driver('facebook')->user();
       $authUser = User::where('facebook_id', $auth_user->id)->first();
       if ($authUser) {
+          $request->session()->put('usuario_correo', $authUser->email);
+          $request->session()->put('usuario_nombre', $authUser->nombre);
           Auth::login($authUser, true);
-          return redirect('/');
+          return redirect('/vuelos');
       }
       else{
         $data = (object)array('nombre' => $auth_user->name, 'facebook_id' => $auth_user->id, 'email' => $auth_user->email);
@@ -30,7 +32,7 @@ class facebookController extends Controller
         $request->session()->put('usuario_correo', $auth_user->email);
         $request->session()->put('usuario_nombre', $auth_user->name);
         Auth::login($user, true);
-        return redirect('/');
+        return redirect('/vuelos');
       }
   }
 }
